2b80bc3a2bd953275c18efbb68d3d6480d50902d
Ananta Widyaswara <anantawidy@gmail.com>
2024-09-06 13:50:11 +0700

    PR10

---
+++ PR10.java
@@ -0,0 +1,16 @@
+public class PR10 {
+    public static void main(String[] args) {
+        String[] carList = new String[6];
+
+        carList[0] = "Mitsubishi";
+        carList[1] = "Tesla";
+        carList[2] = "Honda";
+        carList[3] = "Nissan";
+        carList[4] = "KIA";
+        carList[5] = "Hyundai";
+
+        System.out.println("Content of Car List : " + carList[5]);
+        System.out.println("Content of Car List : " + carList[1]);
+        System.out.println("Content of Car List : " + carList[3]);
+    }
+}
